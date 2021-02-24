c3e243b73771a11e02d42aa797e6ec6964c5baf5
Adupav <adupav2000@gmail.com>
2021-02-24 22:43:35 +0100

    put the map function into raycasting makes much more sense

--- config/map.c
+++ config/map.c
@@ -6,49 +6,35 @@
 /*   By: AlainduPavillon <marvin@42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/18 10:06:56 by AlainduPa         #+#    #+#             */
-/*   Updated: 2021/02/04 05:48:45 by AlainduPa        ###   ########.fr       */
+/*   Updated: 2021/02/24 22:39:20 by AlainduPa        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-char ft_map(t_str *map, int width, int height)
-{
-    t_str *map_line;
-
-    map_line = map;
-    while (map_line && height--)
-       map_line = map_line->next; 
-    if (map_line && width <= ft_strlen(map_line->line))
-        return (map_line->line[width]);
-    return (-1);
-}
-
 int parse_map_line(t_map *map, char *line)
 {
     int i;
     t_str *map_list;
-
-    if (!line[0])
-        return (-1);
+    
     if (map->plan == NULL)
     {
         if (!(map->plan = malloc(sizeof(t_str))) ||
             !(map->plan->line = (char *)malloc(sizeof(ft_strlen(line)) + 1)))
             return (-1);
         ft_strlcpy(map->plan->line, line, ft_strlen(line));
-        map->plan->next = NULL;
         printf("first line : %s\n", map->plan->line);
+        map->plan->next = NULL;
         return (0);
     }
     map_list = map->plan;
-    while (map_list)
-       map_list = map_list->next;
+    while (map_list != NULL)
+        map_list = map_list->next;
     if (!(map_list = (t_str *)malloc(sizeof(t_str))) || 
-            !(map_list->line = (char *)malloc(sizeof(ft_strlen(line)) + 1)))
+            !(map_list->line = (char *)malloc(ft_strlen(line) + 1)))
         return (-1);
     map_list->next = NULL;
-    ft_strlcpy(map_list->line, line, ft_strlen(line));
-    printf("line : %s\n", map_list->line);
+    ft_strlcpy(map_list->line, line, (ft_strlen(line) + 1));
+    printf("next line : %s\n", map_list->line);
     return (0);
 }
--- engine/raycasting.c
+++ engine/raycasting.c
@@ -6,23 +6,67 @@
 /*   By: AlainduPavillon <marvin@42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/18 20:32:11 by AlainduPa         #+#    #+#             */
-/*   Updated: 2020/12/23 13:35:40 by AlainduPa        ###   ########.fr       */
+/*   Updated: 2021/02/24 22:35:00 by AlainduPa        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-int drawVertLineFromColor(t_img *img_to_change, int x, int y_begin, int len, 
+int drawVertLineFromColor(t_img img_to_change, int x, int y_begin, int len, 
         int color)
 {
-    if ((x > img_to_change->width) || (x < 0) ||(y_begin < 0) 
-            ||(len < 0) ||((len + y_begin) > img_to_change->height))
+    if ((x > img_to_change.width) || (x < 0) ||(y_begin < 0) 
+            ||(len < 0) ||((len + y_begin) > img_to_change.height))
         return (-1);
     while (len)
     {
-        my_mlx_pixel_put(img_to_change, x, (y_begin + len--), color);
+        my_mlx_pixel_put(&img_to_change, x, (y_begin + len--), color);
     }
     return (0);
+}
+
+/*
+ * functions returns the character at the given height and width
+ * the map is given in a t_str linked list
+ * */
+char   ft_map(t_str *map, int width, int height)
+{
+    printf("map_line : map-line%s\n", map->line); 
+    while (map && --height)
+       map = map->next; 
+    if (map && width <= ft_strlen(map->line))
+        return (map->line[width]);
+    printf("returned a -1 SHIT!\n");
+    return (-1);
+}
+
+/*
+ *for the moment it performs dda, this is all what we know
+ * */
+void perform_dda(t_game *game)
+{ 
+    t_player *play;
+
+    play = &(game->player);
+    printf("play->mapX %d, play->mapY) %d\n",  play->mapX, play->mapY);
+    while (play->hit == 0 && ft_map(game->map_info.plan, play->mapX, play->mapY) != -1)
+    {
+        printf("in perform dda : play->sideDistY: %f, play->sideDistX: %f; play->DeltaDistX : %f;play->DeltaDistY : %f\n", play->sideDistY, play->sideDistX, play->deltaDistX, play->deltaDistY); 
+        if (play->sideDistX < play->sideDistY) 
+        {
+            play->sideDistX += play->deltaDistX;
+            play->mapX += play->stepX;
+            play->side = 0;
+        }
+        else
+        {
+            play->sideDistY += play->deltaDistY;
+            play->mapY += play->stepY;
+            play->side = 1;
+        }
+        if (ft_map(game->map_info.plan, play->mapX, play->mapY) > '0')
+            play->hit = 1;
+    }
 }
 
 int raycasting(t_game *game)
@@ -30,18 +74,32 @@ int raycasting(t_game *game)
     int x;
     t_player *play;
 
-    play = game->player;
-    play->current_image->img = mlx_new_image(game->mlx->mlx_ptr,
-            game->map_info->window_width, game->map_info->window_width);
-    play->current_image->addr = mlx_get_data_addr(play->current_image->img, 
-            &play->current_image->bpp, &play->current_image->line_length,
-            &play->current_image->endian);
+    printf("tracking the segfault\n");
+    play = &(game->player);
+    printf("1. tracking the segfault\n");
+    play->current_image.img = mlx_new_image(game->mlx.mlx_ptr,
+            game->map_info.window_width, game->map_info.window_height);
+    printf("2. tracking the segfault\n");
+    play->current_image.height = game->map_info.window_height;
+    printf("3. tracking the segfault\n");
+    play->current_image.width = game->map_info.window_width;
+    printf("4. tracking the segfault\n");
+    play->current_image.addr = mlx_get_data_addr(play->current_image.img, 
+            &play->current_image.bpp, &play->current_image.line_length,
+            &play->current_image.endian);
+    printf("5. tracking the segfault\n");
     x = 0;
-    while (x < game->map_info->window_width)
+    // w is the position that goes throught every vertical stripe 
+    while (x < game->map_info.window_width)
     {
-        play->cameraX = 2 * x / (double)game->map_info->window_width - 1;         
+        printf("hello\n");
+        // default position of the player
+        play->mapX = (int)play->posX;
+        play->mapY = (int)play->posY;
+        play->cameraX = 2 * x / (double)game->map_info.window_width - 1;         
         play->rayDirX = play->dirX + play->planeX * play->cameraX;
         play->rayDirY = play->dirY + play->planeY * play->cameraX;
+        printf("play->RayDirX: %f;play->RayDirY : %f\n", play->rayDirX, play->rayDirY);
         if (play->rayDirX == 0)
         {
             play->deltaDistY = 0;
@@ -54,8 +112,10 @@ int raycasting(t_game *game)
         }
         if (play->rayDirY != 0 && play->rayDirX != 0)
         {
-            play->deltaDistX = fabs(1 / play->rayDirY); 
-            play->deltaDistY = fabs(1 / play->rayDirX);
+            play->deltaDistX = (play->rayDirY == 0) ? 
+                0 : ((play->rayDirX == 0) ? 1 : fabs(1 / play->rayDirX));
+            play->deltaDistY = (play->rayDirX == 0) ? 
+                0 : ((play->rayDirY == 0) ? 1 : fabs(1 / play->rayDirY));
         }
         if (play->rayDirX < 0)
         {
@@ -77,45 +137,31 @@ int raycasting(t_game *game)
             play->stepY = 1;
             play->sideDistY = (play->mapY + 1.0 - play->posY) * play->deltaDistY;
         }
-        while (play->hit == 0)
-        {
-            if (play->sideDistX < play->sideDistY) 
-            {
-                play->sideDistX += play->deltaDistX;
-                play->mapX += play->stepX;
-                play->side = 0;
-            }
-            else
-            {
-                play->sideDistY += play->deltaDistY;
-                play->mapY += play->stepY;
-                play->side = 1;
-            }
-            if (game->map_info->plan[play->mapX][play->mapY] > '0')
-                play->hit = 1;
-        }
+        
+        perform_dda(game);
         if (play->side == 0)
             play->perpWallDist = (play->mapX - play->posX + 
                     (1 - play->stepX) / 2) / play->rayDirX;
         else
             play->perpWallDist = (play->mapY - play->posY +
                    (1 - play->stepY) / 2) / play->rayDirY;
-        play->lineHeight = (int)(game->map_info->window_height / play->perpWallDist);
-        play->drawStart = -play->lineHeight / 2 + game->map_info->window_height; 
+        play->lineHeight = (int)(game->map_info.window_height / play->perpWallDist);
+        play->drawStart = -play->lineHeight / 2 + game->map_info.window_height; 
         if (play->drawStart < 0)
             play->drawStart = 0;
-        play->drawEnd = play->lineHeight / 2 + game->map_info->window_height / 2;
-        if (play->drawEnd >= game->map_info->window_height)
-            play->drawEnd = game->map_info->window_height - 1;
-        play->wallColor = game->map_info->color_floor;
+        play->drawEnd = play->lineHeight / 2 + game->map_info.window_height / 2;
+        if (play->drawEnd >= game->map_info.window_height)
+            play->drawEnd = game->map_info.window_height - 1;
+        play->wallColor = game->map_info.color_floor;
         if (play->side == 1)
            play->wallColor = (play->wallColor / 2);
+        printf("play->drawStart : %d; play->drawStart : %d;", play->drawStart, play->drawEnd);
         if (drawVertLineFromColor(play->current_image, x, play->drawStart, 
                 (play->drawEnd - play->drawStart), play->wallColor))
             return (-1);
         x++;
     }
-    mlx_put_image_to_window(game->mlx->mlx_ptr, game->mlx->mlx_win, 
-            play->current_image->img, 0, 0);
+    mlx_put_image_to_window(game->mlx.mlx_ptr, game->mlx.mlx_win, 
+            play->current_image.img, 0, 0);
     return (0);
 }
