348a8ecaffa96fba9eb088f2738ed684c32c1f66
Adupav <adupav2000@gmail.com>
2021-04-16 14:44:02 +0200

    C'est vrm tla merde

--- cub3d.h
+++ cub3d.h
@@ -6,255 +6,233 @@
 /*   By: adu-pavi <adu-pavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/18 09:05:22 by AlainduPa         #+#    #+#             */
-/*   Updated: 2021/04/15 17:05:52 by adu-pavi         ###   ########.fr       */
+/*   Updated: 2021/04/16 14:43:01 by adu-pavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
-#include <math.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <string.h>
-#include "mlx/mlx.h"
+# include <math.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <string.h>
+# include "mlx/mlx.h"
 
-#include "libft/libft.h"
-#include "utils/keycode_linux.h"
+# include "libft/libft.h"
+# include "utils/keycode_linux.h"
 
-#define MLX_SYNC_IMAGE_WRITABLE    1
-#define MLX_SYNC_WIN_FLUSH_CMD     2
-#define MLX_SYNC_WIN_CMD_COMPLETED 3
+# define MLX_SYNC_IMAGE_WRITABLE 1
+# define MLX_SYNC_WIN_FLUSH_CMD 2
+# define MLX_SYNC_WIN_CMD_COMPLETED 3
 
-#define X_EVENT_EXIT 17
+# define X_EVENT_EXIT 17
 
-
-typedef struct  s_img {
-    void        *img;
-    char        *addr;
-    int         bpp;
-    int         line_length;
-    int         width;
-    int         height;
-    int         endian;
-}               t_img;
+typedef struct	s_img
+{
+	void	*img;
+	char	*addr;
+	int		bpp;
+	int		line_length;
+	int		width;
+	int		height;
+	int		endian;
+}				t_img;
 
 typedef struct	s_sprite
 {
-	double			    posX;
-    double             posY;
+	double			posX;
+	double			posY;
 	double			distance;
 	t_img			*tex;
 	struct s_sprite	*next;
 	struct s_sprite	*sorted;
-}	t_sprite;
+}				t_sprite;
 
-typedef struct s_sprite_print  
+typedef struct s_sprite_print
 {
-   /*sprite vars*/
-    double spriteX;
-    double spriteY;
-    double invDet;
-    double transformX;
-    double transformY;
-    int     spriteScreenX;
-    int     spriteWidth;
-    int     spriteHeight;
-    int     drawStartY;
-    int     drawEndY;
-    int     drawStartX;
-    int     drawEndX;
-    int     texX;
-    int     texY;
-    int     texWidth;
-    int     texHeight;
-} t_sprite_print;
+	double	spriteX;
+	double	spriteY;
+	double	invDet;
+	double	transformX;
+	double	transformY;
+	int		spriteScreenX;
+	int		spriteWidth;
+	int		spriteHeight;
+	int		drawStartY;
+	int		drawEndY;
+	int		drawStartX;
+	int		drawEndX;
+	int		texX;
+	int		texY;
+	int		texWidth;
+	int		texHeight;
+}				t_sprite_print;
 
-typedef struct    data_s
+typedef struct data_s
 {
-    void          *mlx_ptr;
-    void          *mlx_win;
-}                 t_data;
+	void *mlx_ptr;
+	void *mlx_win;
+}				t_data;
 
-typedef struct s_str{
-   char          *line;
-   struct s_str  *next;
-}   t_str;
+typedef struct s_str
+{
+	char			*line;
+	struct s_str	*next;
+} t_str;
 
-typedef struct s_pos{
-    double  x;
-    double  y;
+typedef struct s_pos
+{
+	double x;
+	double y;
 } t_pos;
 
-typedef struct s_player {
-    int position_initialised;
-    /*the users position*/
-    double posX;
-    double posY;
-    /*the direction the user is watching*/
-    double dirX;
-    double dirY;
-    /*camera plane of the map*/
-    double planeX;
-    double planeY;
-    /*time of the current frame*/ 
-    double time;
-    /*time of the old frame*/
-    double old_time;
-    /*the x-coordinate of */
-    double cameraX;
-    /*rays X anarwsind Y*/
-    double rayDirX;
-    double rayDirY;
-    /*position in the map out of context*/
-    int mapX;
-    int mapY;
-    /*distance to the first next wall(x-side or y-side)*/
-    double sideDistX;
-    double sideDistY;
-    /*length between two sides*/
-    double deltaDistX;
-    double deltaDistY;
-    /*used to calculate the length of the ray*/
-    double perpWallDist;
-    /*get the direction of the steps*/
-    int stepX;
-    int stepY;
-    /*if a wall was hit*/
-    int hit;
-    /*which side of the wall was hit*/
-    int side;
-    /*height that will be drawn on the screen*/
-    int lineHeight; 
-    int drawStart;
-    int drawEnd;
-    /*color of the wall that is being drawn right after*/
-    int wallColor;
-    /*Image used as buffer to be written in the page*/
-    t_img current_image;
-    /*MOVING AND ROTATING VARIABLES*/
-    /*these variables tell if the player is holding a key_down*/
-	/*move forward : moveX = 1*/
-    /*move backward : moveX = -1*/
-    int moveX;
-    int moveY;
-    /*rotation variables*/
-    int rot_left;
-    int rot_right;
-    /*where exactly the wall was hit*/
-    double wallX;
-    /*texturing calculation*/
-    int texNum;
-    int texX;
-    int texY;
-    /*how much to incerease the texture coordinate per screen pixel*/
-    double step;
-    double texPos;
- } t_player;
+typedef struct s_player
+{
+	int position_initialised;
+	double posX;
+	double posY;
+	double dirX;
+	double dirY;
+	double planeX;
+	double planeY;
+	double time;
+	double old_time;
+	double cameraX;
+	double rayDirX;
+	double rayDirY;
+	int mapX;
+	int mapY;
+	double sideDistX;
+	double sideDistY;
+	double deltaDistX;
+	double deltaDistY;
+	double perpWallDist;
+	int stepX;
+	int stepY;
+	int hit;
+	int side;
+	int lineHeight;
+	int drawStart;
+	int drawEnd;
+	int wallColor;
+	t_img current_image;
+	int moveX;
+	int moveY;
+	int rot_left;
+	int rot_right;
+	double wallX;
+	int texNum;
+	int texX;
+	int texY;
+	double step;
+	double texPos;
+} t_player;
 
-typedef struct s_map{   
-    int     window_height;
-    int     window_width;
+typedef struct s_map
+{
+	int window_height;
+	int window_width;
 
-	int     window_max_height;
-    int     window_max_width;
+	int window_max_height;
+	int window_max_width;
 
-    int     color_floor;
-    int     color_ceiling;
+	int color_floor;
+	int color_ceiling;
 
-    int     te_no_color;
-    int     te_so_color;
-    int     te_we_color;
-    int     te_ea_color;
-    int     te_s_color;
+	int te_no_color;
+	int te_so_color;
+	int te_we_color;
+	int te_ea_color;
+	int te_s_color;
 
-    t_img   te_no;
-    t_img   te_so;
-    t_img   te_we;
-    t_img   te_ea;
-    t_img   te_s;
+	t_img te_no;
+	t_img te_so;
+	t_img te_we;
+	t_img te_ea;
+	t_img te_s;
 
-    t_str   *plan;
-    int     plan_height;
-    int     plan_width;
+	t_str *plan;
+	int plan_height;
+	int plan_width;
 
-    t_sprite	*sprites;
+	t_sprite *sprites;
 } t_map;
 
-typedef struct s_config{
-    int     screenshot;
-    char    *prog_name;
-    char    *conf_file;
-	char	**conf_check;
+typedef struct s_config
+{
+	int screenshot;
+	char *prog_name;
+	char *conf_file;
+	char **conf_check;
 } t_config;
 
-typedef struct s_game{
-    t_map           map_info;
-    t_player        player;
-    t_data          mlx;
-    t_config        config;
-    t_sprite_print  sprites_p;
+typedef struct s_game
+{
+	t_map map_info;
+	t_player player;
+	t_data mlx;
+	t_config config;
+	t_sprite_print sprites_p;
 } t_game;
 
-void            my_mlx_pixel_put(t_img *data, int x, int y, int color);
-    
-int     parsing(t_game *game, int argc, char **argv);
+void my_mlx_pixel_put(t_img *data, int x, int y, int color);
 
-int     handle_resolution(char *line, t_map *map_info, t_game *game);
+int parsing(t_game *game, int argc, char **argv);
 
-int     handle_textures(char **line, t_map *map_info, t_data *data);
+int handle_resolution(char *line, t_map *map_info, t_game *game);
 
-int     load_texture(char *tex_link, t_img *img, t_data *mlx);
+int handle_textures(char **line, t_map *map_info, t_data *data);
 
-int     parse_line(char *line, t_data *data, t_map *map_info, t_game *game);
+int load_texture(char *tex_link, t_img *img, t_data *mlx);
 
-int     handle_colors(char **line, t_map *map_info);
+int parse_line(char *line, t_data *data, t_map *map_info, t_game *game);
 
-int     parse_map_line(t_game *game, t_map *map, char *line);
+int handle_colors(char **line, t_map *map_info);
 
-char    ft_map(t_str *map, int width, int height);
-void    set_wall_color(t_game *game, int x);
+int parse_map_line(t_game *game, t_map *map, char *line);
 
-int    draw_floor_and_ceiling(t_img img_to_change, int x, int y_begin, int len,
-            t_map *map_info);
-int     drawVertLineFromColor(t_img img_to_change, int x, int y_begin, int len, 
-            int color);
-int     raycasting(t_game *game);
+char ft_map(t_str *map, int width, int height);
+void set_wall_color(t_game *game, int x);
 
-int     key_press(int key_code, t_game *game);
-int     key_release(int key_code, t_game *game);
+int draw_floor_and_ceiling(t_img img_to_change, int x, int y_begin, int len,
+						   t_map *map_info);
+int drawVertLineFromColor(t_img img_to_change, int x, int y_begin, int len,
+						  int color);
+int raycasting(t_game *game);
 
-void    perform_dda(t_game *game);
-void    update_pos_view(t_game *game);
-void    update_rotation(t_game *game);
+int key_press(int key_code, t_game *game);
+int key_release(int key_code, t_game *game);
 
-int     everything_was_set(t_map *map_info, t_game *game);
+void perform_dda(t_game *game);
+void update_pos_view(t_game *game);
+void update_rotation(t_game *game);
 
-void    init_player(t_player *player);
-void    init_map(t_map *map_info);
-void	init_config(t_config *config);
+int everything_was_set(t_map *map_info, t_game *game);
 
-/*all exit and clearing functions*/
-void    clear_player(t_game *game);
-void    clear_mlx(t_data *mlx);
-void    clear_map_info(t_game *game);
-void    clear_config(t_config *config);
-void    clear_all_variables(t_game *game);
-int     exit_error(t_game *game, char *message);
-int     exit_success(t_game *game);
+void init_player(t_player *player);
+void init_map(t_map *map_info);
+void init_config(t_config *config);
 
-int     write_and_save_screen(t_game *game);
+void clear_player(t_game *game);
+void clear_mlx(t_data *mlx);
+void clear_map_info(t_game *game);
+void clear_config(t_config *config);
+void clear_all_variables(t_game *game);
+int exit_error(t_game *game, char *message);
+int exit_success(t_game *game);
+
+int write_and_save_screen(t_game *game);
 
 int load_color_from_tex(t_img *tex, int x, int y);
 void texture_pixel_put(t_game *game, t_img *tex, int x, int y);
 void sprite_pixel_put(t_game *game, t_img *tex,
-	t_sprite_print *sprit, int x, int y);
+					  t_sprite_print *sprit, int x, int y);
 
 int sprite_bubble_sort(t_game *game);
 double get_ss_distance(double posX, double posY, int posSX, int posSY);
 int set_sprite_distance(t_game *game);
 
-/*main function*/
-int     main(int argc, char **argv);
+int main(int argc, char **argv);
 
 void print_all_sprite(t_game *game);
 #endif
--- engine/sprites.c
+++ engine/sprites.c
@@ -6,76 +6,79 @@
 /*   By: adu-pavi <adu-pavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/12 22:32:13 by adu-pavi          #+#    #+#             */
-/*   Updated: 2021/04/14 13:41:44 by adu-pavi         ###   ########.fr       */
+/*   Updated: 2021/04/16 14:31:30 by adu-pavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../cub3d.h"
 
-int sprite_bubble_sort(t_game *game)
+void	sprite_bubble_sort_1(t_sprite *sprite_tmp, t_sprite *sprite_prev,
+						t_sprite *sprite_origin, int unsorted)
 {
-    int unsorted;
-    t_sprite *sprite_tmp;
-    t_sprite *sprite_prev;
-    t_sprite *sprite_origin;
-
-    unsorted = 1;
-    while (unsorted != 0)
-    {
-        unsorted = 0; 
-        sprite_origin = game->map_info.sprites;
-        if (sprite_origin->distance < sprite_origin->next->distance)
-        {
-            game->map_info.sprites = game->map_info.sprites->next;
-            sprite_origin->next = game->map_info.sprites->next;
-            game->map_info.sprites->next = sprite_origin;
-            sprite_prev = game->map_info.sprites; 
-        }
-        else
-        {
-            sprite_origin = game->map_info.sprites->next;  
-            sprite_prev = game->map_info.sprites ;
-        }
-        while (sprite_origin != NULL && sprite_origin->next != NULL)
-        {
-            // print_all_sprite(game);
-            if (sprite_origin->distance >= sprite_origin->next->distance)
-            {
-                sprite_prev = sprite_origin;
-                sprite_origin = sprite_origin->next;
-            }
-            else
-            {
-                unsorted = 1;
-                sprite_prev->next = sprite_origin->next;
-                sprite_origin->next = sprite_origin->next->next;
-                sprite_prev->next->next = sprite_origin;
-                sprite_prev = sprite_origin;
-                sprite_origin = sprite_origin->next;
-            }
-        }
-
-    }
-    return (0);
+	while (sprite_origin != NULL && sprite_origin->next != NULL)
+	{
+		if (sprite_origin->distance >= sprite_origin->next->distance)
+		{
+			sprite_prev = sprite_origin;
+			sprite_origin = sprite_origin->next;
+		}
+		else
+		{
+			unsorted = 1;
+			sprite_prev->next = sprite_origin->next;
+			sprite_origin->next = sprite_origin->next->next;
+			sprite_prev->next->next = sprite_origin;
+			sprite_prev = sprite_origin;
+			sprite_origin = sprite_origin->next;
+		}
+	}
 }
 
-double get_ss_distance(double posX, double posY, int posSX, int posSY)
+int		sprite_bubble_sort(t_game *game)
 {
-    return ((posX - posSX) * (posX - posSX) 
-        + (posY - posSY) * (posY - posSY));
+	int			unsorted;
+	t_sprite	*sprite_tmp;
+	t_sprite	*sprite_prev;
+	t_sprite	*sprite_origin;
+
+	unsorted = 1;
+	while (unsorted != 0)
+	{
+		unsorted = 0;
+		sprite_origin = game->map_info.sprites;
+		if (sprite_origin->distance < sprite_origin->next->distance)
+		{
+			game->map_info.sprites = game->map_info.sprites->next;
+			sprite_origin->next = game->map_info.sprites->next;
+			game->map_info.sprites->next = sprite_origin;
+			sprite_prev = game->map_info.sprites;
+		}
+		else
+		{
+			sprite_origin = game->map_info.sprites->next;
+			sprite_prev = game->map_info.sprites;
+		}
+		sprite_bubble_sort_1(sprite_tmp, sprite_prev, sprite_origin, unsorted);
+	}
+	return (0);
 }
 
-int set_sprite_distance(t_game *game)
+double	get_ss_distance(double pos_x, double pos_y, int pos_sx, int pos_sy)
 {
-    t_sprite *sprite;
-
-    sprite = game->map_info.sprites;
-    while (sprite != NULL)    
-    {
-        sprite->distance = get_ss_distance(game->player.posX, 
-            game->player.posY, sprite->posX, sprite->posY);
-        sprite = sprite->next;
-    }
-    return (sprite_bubble_sort(game));
+	return ((pos_x - pos_sx) * (pos_x - pos_sx)
+		+ (pos_y - pos_sy) * (pos_y - pos_sy));
 }
 
+int		set_sprite_distance(t_game *game)
+{
+	t_sprite *sprite;
+
+	sprite = game->map_info.sprites;
+	while (sprite != NULL)
+	{
+		sprite->distance = get_ss_distance(game->player.posX,
+			game->player.posY, sprite->posX, sprite->posY);
+		sprite = sprite->next;
+	}
+	return (sprite_bubble_sort(game));
+}
