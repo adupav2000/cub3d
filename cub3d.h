fb8b9e1a772a72fb1e6ff35d8475f98a65bcbc64
Adupav <adupav2000@gmail.com>
2021-04-20 20:02:52 +0200

    main

--- cub3d.h
+++ cub3d.h
@@ -6,7 +6,7 @@
 /*   By: adu-pavi <adu-pavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/18 09:05:22 by AlainduPa         #+#    #+#             */
-/*   Updated: 2021/04/20 19:35:35 by adu-pavi         ###   ########.fr       */
+/*   Updated: 2021/04/20 19:50:36 by adu-pavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@
 
 # define X_EVENT_EXIT 17
 
-typedef struct	s_img
+typedef struct s_img
 {
 	void	*img;
 	char	*addr;
@@ -38,7 +38,7 @@ typedef struct	s_img
 	int		endian;
 }				t_img;
 
-typedef struct	s_sprite
+typedef struct s_sprite
 {
 	double			posx;
 	double			posy;
@@ -48,7 +48,7 @@ typedef struct	s_sprite
 	struct s_sprite	*sorted;
 }				t_sprite;
 
-typedef struct	s_sprite_print
+typedef struct s_sprite_print
 {
 	double	spritex;
 	double	spritey;
@@ -68,25 +68,25 @@ typedef struct	s_sprite_print
 	int		texheight;
 }				t_sprite_print;
 
-typedef struct	s_data
+typedef struct s_data
 {
-	void *mlx_ptr;
-	void *mlx_win;
+	void	*mlx_ptr;
+	void	*mlx_win;
 }				t_data;
 
-typedef struct	s_str
+typedef struct s_str
 {
 	char			*line;
 	struct s_str	*next;
 }				t_str;
 
-typedef struct	s_pos
+typedef struct s_pos
 {
-	double x;
-	double y;
+	double	x;
+	double	y;
 }				t_pos;
 
-typedef struct	s_player
+typedef struct s_player
 {
 	int		position_initialised;
 	double	posx;
@@ -128,32 +128,32 @@ typedef struct	s_player
 	double	texpos;
 }			t_player;
 
-typedef struct	s_map
+typedef struct s_map
 {
-	int		window_height;
-	int		window_width;
-	int		window_max_height;
-	int		window_max_width;
-	int		color_floor;
-	int		color_ceiling;
-	int		te_no_color;
-	int		te_so_color;
-	int		te_we_color;
-	int		te_ea_color;
-	int		te_s_color;
-	t_img	te_no;
-	t_img	te_so;
-	t_img	te_we;
-	t_img	te_ea;
-	t_img	te_s;
-	t_str	*plan;
-	int		plan_height;
-	int		plan_width;
+	int			window_height;
+	int			window_width;
+	int			window_max_height;
+	int			window_max_width;
+	int			color_floor;
+	int			color_ceiling;
+	int			te_no_color;
+	int			te_so_color;
+	int			te_we_color;
+	int			te_ea_color;
+	int			te_s_color;
+	t_img		te_no;
+	t_img		te_so;
+	t_img		te_we;
+	t_img		te_ea;
+	t_img		te_s;
+	t_str		*plan;
+	int			plan_height;
+	int			plan_width;
 
-	t_sprite *sprites;
+	t_sprite	*sprites;
 }			t_map;
 
-typedef struct	s_config
+typedef struct s_config
 {
 	int		screenshot;
 	char	*prog_name;
@@ -161,7 +161,7 @@ typedef struct	s_config
 	int		conf_check;
 }				t_config;
 
-typedef struct	s_game
+typedef struct s_game
 {
 	t_map			map_info;
 	t_player		player;
--- main.c
+++ main.c
@@ -6,40 +6,41 @@
 /*   By: adu-pavi <adu-pavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/23 12:02:45 by AlainduPa         #+#    #+#             */
-/*   Updated: 2021/04/18 19:25:28 by adu-pavi         ###   ########.fr       */
+/*   Updated: 2021/04/20 19:56:04 by adu-pavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int cross_quit(int key, t_game *game)
+int	cross_quit(int key, t_game *game)
 {
 	(void)key;
 	exit_success(game);
 	return (0);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	t_game        game;
+	t_game	game;
 
-	if ((game.mlx.mlx_ptr = mlx_init()) == NULL)
+	game.mlx.mlx_ptr = mlx_init();
+	if ((game.mlx.mlx_ptr) == NULL)
 		return (exit_error(&game, " mlx init didn't worked."));
 	if (parsing(&game, argc, argv))
 		return (exit_error(&game, " parsing failed"));
-	if ((game.mlx.mlx_win = mlx_new_window(game.mlx.mlx_ptr, 
-					game.map_info.window_width, game.map_info.window_height,
-					argv[0])) == NULL)
+	game.mlx.mlx_win = mlx_new_window(game.mlx.mlx_ptr,
+			game.map_info.window_width, game.map_info.window_height, argv[0]);
+	if (game.mlx.mlx_win == NULL)
 		return (exit_error(&game, "Error loading new window"));
-	mlx_hook(game.mlx.mlx_win, 2, (1L<<0), &key_press, &game);
-	mlx_hook(game.mlx.mlx_win, 3, (1L<<1), &key_release, &game);
+	mlx_hook(game.mlx.mlx_win, 2, (1L << 0), &key_press, &game);
+	mlx_hook(game.mlx.mlx_win, 3, (1L << 1), &key_release, &game);
 	mlx_hook(game.mlx.mlx_win, 17, 0, &cross_quit, &game);
 	mlx_loop_hook(game.mlx.mlx_ptr, raycasting, &game);
 	if (game.config.screenshot && !raycasting(&game))
 		if (write_and_save_screen(&game))
 			return (exit_error(&game, " Screen shot couldn't be saved"));
 	if (game.config.screenshot)
-		return(exit_success(&game));
+		return (exit_success(&game));
 	mlx_loop(game.mlx.mlx_ptr);
 	return (EXIT_SUCCESS);
 }
